71dcab791740e30a400ab5900992a108b08d497f
Clamatius <clamatius@gmail.com>
2025-01-05 18:15:28 -0800

    use custom image with long term memory feature

--- run.sh
+++ run.sh
@@ -6,7 +6,10 @@ if [ "$#" -lt 1 ]; then
     exit 1
 fi
 
+
+
 sweagent run --config config/default.yaml --agent.model.name "claude-3-5-sonnet-20241022" \
 	 --env.repo.path ../llm-memory \
 	 --agent.model.rate_limit=40000 \
+	 --env.deployment.image=memory-swe-rex \
 	 --problem_statement.path=$1
